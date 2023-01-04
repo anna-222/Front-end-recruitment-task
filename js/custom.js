96c68a1f6d58524408ac247d0a8c990ca5d38325
Anna Kolbusz <a.sawinska222@gmail.com>
2023-01-04 20:44:38 +0100

    Add watch section, add popup, add logic js popup

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,15 @@
 /** INSERT CUSTOM JS HERE **/
-export default function() {
+export default function () {
+  const openModalButton = document.getElementById("video__icon");
+  const closeModalButton = document.getElementById("modalVideo_close");
 
+  const modalVideo = document.getElementById("modalVideo");
+
+  openModalButton.addEventListener("click", () => {
+    modalVideo.classList.add("activeModal");
+  });
+
+  closeModalButton.addEventListener("click", () => {
+    modalVideo.classList.remove("activeModal");
+  });
 }
