f0028185e9499c5ac31708a8e20be8d796e973ac
Anna Kolbusz <a.sawinska222@gmail.com>
2023-01-05 09:39:15 +0100

    change menu view

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,7 @@
 export default function () {
   const openModalButton = document.getElementById("video__icon");
   const closeModalButton = document.getElementById("modalVideo_close");
+  const menuItems = document.getElementsByClassName("navbar__list--item");
 
   const modalVideo = document.getElementById("modalVideo");
 
@@ -12,4 +13,12 @@ export default function () {
   closeModalButton.addEventListener("click", () => {
     modalVideo.classList.remove("activeModal");
   });
+
+  for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
 }
